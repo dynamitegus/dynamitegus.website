16f852c5e44f84d552354ef7bc913c927cedffb1
DynamiteGus <angus.rose@hotmail.com>
2024-12-27 16:23:37 +1100

    Adding tailwind becuase i felt like it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dynamitegus.org",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
