aac0009a856c0d2b70abb24aee26151e9ef8f458
dynamitegus <dynamitegus@dynamitegus.org>
2025-01-12 12:15:23 +1100

    Working to try and fix the sorting.

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,8 +2,8 @@ import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
 //thanks to petermorgandev's git repo i understand what im meant to be doing.
 const blogPostsCollection = defineCollection({
-	type: "content",
-    //loader: glob({ pattern: "*.md", base: "./content/blog" }),
+	//type: "content",
+    loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
