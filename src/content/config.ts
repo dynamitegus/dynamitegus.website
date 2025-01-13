b1615df6932f3b8b08b3ea2c98626d40d47b655d
dynamitegus <dynamitegus@dynamitegus.org>
2025-01-13 17:12:54 +1100

    Some Minor tweaks.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,6 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
+import { string } from "astro:schema";
 //thanks to petermorgandev's git repo i understand what im meant to be doing.
 const blogPostsCollection = defineCollection({
 	//type: "content",
@@ -10,6 +11,11 @@ const blogPostsCollection = defineCollection({
 		modDate: z.date().optional(),
 		pubDate: z.date(),
 		tags: z.array(z.string()).optional(),
+		author: z.string().default('dynamitegus'),
+		image:	z.object({
+			url: z.string(),
+			alt: z.string()
+		}).optional()
 	}),
 });
 export const collections = { blog: blogPostsCollection };
