b3bea50c09593676ca4943ae9eb4932ac1d2e7ba
dynamitegus <dynamitegus@dynamitegus.org>
2025-03-14 12:04:39 +1100

    Finish blog post and edit rss.xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
     return rss({
         title: 'dynamitegus | Blog',
         description: 'dynamitegus\'s blog its blogging time',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: await pagesGlobToRssItems(getCollection("blog")),
         customData: `<language>en-us</language>`,
     });
 }
