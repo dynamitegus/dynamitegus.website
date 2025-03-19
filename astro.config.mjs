44c31a316e01a50f5e8d877760a5ce80d7e6ac86
DynamiteGus <angus.rose@hotmail.com>
2025-03-19 20:45:27 +1100

    Ooops Missed One.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
@@ -12,5 +10,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dynamitegus.org",
-  integrations: [preact(), tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react()]
 });
