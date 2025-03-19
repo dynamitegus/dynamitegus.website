99e19eeb5a5c1836b265d9ce65d0685fe09d7691
DynamiteGus <angus.rose@hotmail.com>
2025-03-19 20:23:35 +1100

    Add React.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,10 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dynamitegus.org",
-  integrations: [preact(), tailwind(), mdx()]
+  integrations: [preact(), tailwind(), mdx(), react()]
 });
