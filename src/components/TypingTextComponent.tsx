51b3fd0cf457a713de7b261c5e4f9d75c52f3fe2
DynamiteGus <angus.rose@hotmail.com>
2025-10-13 22:08:47 +1100

    ubuntu mono and other colours i thiink

--- src/components/TypingTextComponent.tsx
+++ src/components/TypingTextComponent.tsx
@@ -1,18 +1,30 @@
-import TypingText from '@/components/ui/shadcn-io/typing-text';
+import TypingText from "@/components/ui/shadcn-io/typing-text";
 
 export default function TypingTextDemo() {
   return (
     <div className="flex items-center justify-center w-full">
-      <TypingText 
-        text={["A Software Developer", "An Engineer", "A Network Nerd", "A Web Developer", "A CFA Volunteer", "some guy", "stfu about telephony", "this is not funny", "why did you give me a business card", "some dude with a 3d printer"]}
+      <TypingText
+        text={[
+          "A Software Developer",
+          "An Engineer",
+          "A Network Nerd",
+          "A Web Developer",
+          "A CFA Volunteer",
+          "Some Guy",
+          "stfu about telephony",
+          "This is not funny.",
+          "Some dude with a 3D printer",
+          "Not Hatsune Miku",
+        ]}
         typingSpeed={75}
         pauseDuration={1500}
         showCursor={true}
-        className="text-4xl font-bold text-center max-w-8xl"
+        className="text-4xl font-mono text-center max-w-8xl"
         cursorClassName="h-12"
-        textColors={['#3b82f6', '#8b5cf6', '#06b6d4']}
+        textColors={["#dc8a78", "#dd7878", "#ea76cb", "#ff00ff", "#8839ef", "#d20f39", "#e64553", "#fe640b", "#df8e1d", "#40a02b", "#00ff00", "#179299", "#04a5e5", "#209fb5", "#1e66f5", "#7287fd"]}
         variableSpeed={{ min: 50, max: 120 }}
       />
     </div>
   );
 }
+
